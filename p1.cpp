f8c46887e2b9ce463c369e2e58a10bcf1a0933a0
Salvador Eliot Hilares Barrios <39841982+SalvadorHilares@users.noreply.github.com>
2022-03-25 17:03:55 -0500

    Add files via upload

--- p1.cpp
+++ p1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
 
 struct Alumno{
@@ -9,13 +10,14 @@ struct Alumno{
     char carrera [15];
 };
 
-istream & operator >> (istream & stream, Alumno & p){	
-	stream.read(p.codigo, 5);		
-	stream.read(p.nombre, 11);		
-	stream.read(p.apellidos, 20);		
-	stream.read(p.carrera, 15);
-    	stream.get(); // read \n	
-	return stream;
+istream & operator>> (istream & stream, Alumno & record){
+    stream.read(record.codigo, 5);
+    stream.read(record.nombre, 11);
+    stream.read(record.apellidos, 20);
+    stream.read(record.carrera, 15);
+    stream.get(); //read \n
+    stream.get();
+    return stream;
 }
 
 class FixedRecord{
@@ -26,8 +28,18 @@ class FixedRecord{
             this->name = name;
         }
         vector<Alumno> load(){
-            //TODO: INGRESAR CODIGO
+
+            ifstream archivo;
+            archivo.open(name, ios::in | ios::app);
+
             vector<Alumno> alumnos;
+            Alumno alumno;
+            
+            while(archivo){
+                archivo >> alumno;
+                alumnos.push_back(alumno);
+            }
+            archivo.close();
             return alumnos;      
         }
         void add(Alumno record){
@@ -40,5 +52,8 @@ class FixedRecord{
 };
 
 int main(){
+    class FixedRecord record("datos1.txt");
+    vector<Alumno> alumnos = record.load();
+    cout<<alumnos.size()<<endl;
     return 0;
 }

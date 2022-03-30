b274ded33bb0259d861a714ded2ec8cdf3e2e5f5
rodricar99 <39935861+rodricar99@users.noreply.github.com>
2022-03-29 22:49:44 -0500

    123

---
+++ cmake-build-debug/CMakeFiles/DATABASEII.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DATABASEII.dir/p1.cpp.obj"
+  "DATABASEII.exe"
+  "DATABASEII.exe.manifest"
+  "DATABASEII.pdb"
+  "libDATABASEII.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DATABASEII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
